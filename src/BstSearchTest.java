a4909a2ad45b2125bf417df44a27edb4676cd3db
rjfredr <fredrickalexander1727@gmail.com>
2025-10-28 11:55:50 -0700

    new tests

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,16 +2,40 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest<T> {
+    BinaryTreeNode<Integer> rootIntComplex = new BinaryTreeNode<Integer>(2,
+        new BinaryTreeNode<>(1, null, null),
+        new BinaryTreeNode<>(3, null, null));
+
     BinaryTreeNode<Integer> rootInt = new BinaryTreeNode<Integer>(2,
         new BinaryTreeNode<>(1, null, null),
         new BinaryTreeNode<>(3, null, null));
 
-    BinaryTreeNode<Character> rootNum = new BinaryTreeNode<Character>('b',
+    BinaryTreeNode<Character> rootChar = new BinaryTreeNode<Character>('b',
         new BinaryTreeNode<>('a', null, null),
         new BinaryTreeNode<>('c', null, null));
 
     @Test
     public void testIfOneExistsInTree() {
-        
+        BstSearch search = new BstSearch();
+        Boolean actual = search.contains(rootInt, 1);
+        assertTrue(actual);
+    }
+    @Test
+    public void testIfFourExistsInTree() {
+        BstSearch search = new BstSearch();
+        Boolean actual = search.contains(rootInt, 4);
+        assertFalse(actual);
+    }
+    @Test
+    public void testIfCharAExistsInTree() {
+        BstSearch search = new BstSearch();
+        Boolean actual = search.contains(rootChar, 'a');
+        assertTrue(actual);
+    }
+    @Test
+    public void testIfCharDExistsInTree() {
+        BstSearch search = new BstSearch();
+        Boolean actual = search.contains(rootChar, 'd');
+        assertFalse(actual);
     }
 }
