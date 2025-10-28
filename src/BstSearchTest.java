9b217bc64803d6210e0300ff3d5d24d4ae380aed
ShawnN003 <nguru.shawn@student.greenriver.edu>
2025-10-28 14:30:53 -0500

    more test cases implemented

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -25,4 +25,29 @@ public class BstSearchTest {
         // Assert
         assertTrue(result);
     }
+    @Test
+    public void testBinarySearchFalseTesting() {
+        // Arrange
+        BinaryTreeNode balancedTree = new BinaryTreeNode<>(40,      //root
+                                        new BinaryTreeNode<>(12,null,null),     //right
+                                            new BinaryTreeNode<>(45,null,null));       //left
+        
+        // Act
+        boolean result = BstSearch.contains(balancedTree, 100);
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    public void testBinaryBigSearchTesting() {
+        // Arrange
+        BinaryTreeNode bigBinaryTreeNode = new BinaryTreeNode<>(40,      //root
+                                        new BinaryTreeNode<>(12,new BinaryTreeNode<>(5,null,null),new BinaryTreeNode<>(15,null,null)),     //right
+                                            new BinaryTreeNode<>(8,null,null));       //left
+        
+        // Act
+        boolean result = BstSearch.contains(bigBinaryTreeNode, 5);
+        // Assert
+        assertTrue(result);
+    }
 }
