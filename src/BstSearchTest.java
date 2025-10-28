bc43a21dff793efbffb9041e985d696cd7de2b8c
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-10-28 11:58:33 -0700

    test up

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 public class BstSearchTest {
     @Test
@@ -22,5 +23,29 @@ public class BstSearchTest {
         BinaryTreeNode<String> root = new BinaryTreeNode<>("GOD", leftLeaf, rightLeaf);
 
         assertTrue(BstSearch.contains(root, "Dani"));
+    }
+
+    @Test
+    void NullTarget(){
+        BinaryTreeNode<String> leftLeaf = new BinaryTreeNode<>("Dani");
+        BinaryTreeNode<String> rightLeaf = new BinaryTreeNode<>("Brady");
+        
+        // Create a root node with left and right children
+        BinaryTreeNode<String> root = new BinaryTreeNode<>("GOD", leftLeaf, rightLeaf);
+
+        assertThrows(NullPointerException.class, () -> {
+            BstSearch.contains(root, null);
+        });
+    }
+
+    @Test
+    void NullRoot(){
+        BinaryTreeNode<String> leftLeaf = new BinaryTreeNode<>("Dani");
+        BinaryTreeNode<String> rightLeaf = new BinaryTreeNode<>("Brady");
+        
+        // Create a root node with left and right children
+        BinaryTreeNode<String> root = new BinaryTreeNode<>(null, leftLeaf, rightLeaf);
+
+        assertFalse(BstSearch.contains(null, "Brady"));
     }
 }
