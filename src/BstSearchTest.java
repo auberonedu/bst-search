47dcfee6f4c72f54df3e79995471cd132e411204
ShawnN003 <nguru.shawn@student.greenriver.edu>
2025-10-28 13:47:23 -0500

    First test case implemented and its false! (it was supposed to be true)

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
-
+    @Test
+    public void testBinarySearchTesting() {
+        // Arrange
+        BinaryTreeNode balancedTree = new BinaryTreeNode<>(40,      //root
+                                        new BinaryTreeNode<>(12,null,null),     //right
+                                            new BinaryTreeNode<>(45,null,null));       //left
+        
+        // Act
+        boolean result = BstSearch.contains(balancedTree, 45);
+        // Assert
+        assertTrue(result);
+    }
 }
