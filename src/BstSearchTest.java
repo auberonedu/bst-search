097cabbaad65adf202cf572a3a631df097d6f454
rjfredr <fredrickalexander1727@gmail.com>
2025-10-28 11:49:26 -0700

    intializing tests

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -1,6 +1,17 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-public class BstSearchTest {
+public class BstSearchTest<T> {
+    BinaryTreeNode<Integer> rootInt = new BinaryTreeNode<Integer>(2,
+        new BinaryTreeNode<>(1, null, null),
+        new BinaryTreeNode<>(3, null, null));
 
+    BinaryTreeNode<Character> rootNum = new BinaryTreeNode<Character>('b',
+        new BinaryTreeNode<>('a', null, null),
+        new BinaryTreeNode<>('c', null, null));
+
+    @Test
+    public void testIfOneExistsInTree() {
+        
+    }
 }
