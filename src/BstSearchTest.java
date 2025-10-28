5f96dceb1089b37e9a8450d3fd21c0eb5d924028
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-10-28 11:37:11 -0700

    added test for integer

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+    @Test
+    void BstSearch_Integer(){
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(3);
+        BinaryTreeNode<Integer> left = new BinaryTreeNode<Integer>(1);
+        BinaryTreeNode<Integer> right = new BinaryTreeNode<Integer>(4);
+        root.left = left;
+        root.right = right;
 
+        boolean expected = BstSearch.contains(root, 4);
+
+        assertEquals(expected, true);
+        
+    }
 }
