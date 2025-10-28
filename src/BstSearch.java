91cfa263d71cc63e392dae9fc4ec940b4552ead9
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-10-28 11:33:32 -0700

    basic logic

--- src/BstSearch.java
+++ src/BstSearch.java
@@ -10,13 +10,27 @@ public class BstSearch {
      * binary search tree. A null root will cause this method to always return
      * false if a valid target is given.
      * 
-     * @param <T> The type of the values held in the nodes
-     * @param root the root of the binary search tree (assumed to be valid)
+     * @param <T>    The type of the values held in the nodes
+     * @param root   the root of the binary search tree (assumed to be valid)
      * @param target the value to search for
      * @return true if the target is contained in the tree, false otherwise
      * @throws NullPointerException if target is null
      */
     public static <T extends Comparable<T>> boolean contains(BinaryTreeNode<T> root, T target) {
-        return false;
+        if (target == null) {
+            throw new NullPointerException();
+        }
+
+        if (root.data == target) {
+            return true;
+        }
+
+        if (target < root.data) {
+            return contains(root.left, target);
+        }
+
+        if (target > root.data) {
+            return contains(root.right, target);
+        }
     }
 }
