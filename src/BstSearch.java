53635a05dd21265fff6528fd6a739b2bc2e7c092
Auberon López <auberon.lopez@greenriver.edu>
2025-10-28 09:31:13 -0700

    Initial question

---
+++ src/BstSearch.java
@@ -0,0 +1,21 @@
+public class BstSearch {
+    /**
+     * Returns whether a binary search tree contains a given value.
+     * 
+     * As a precondition, the input root is assumed to be the root of a binary
+     * search tree; this is not validated by the method. Passing in an invalid
+     * tree causes undefined behavior.
+     * 
+     * A null value for the root is considered to represent a valid but empty
+     * binary search tree. A null root will cause this method to always return
+     * false.
+     * 
+     * @param <T> The type of the values held in the nodes
+     * @param root the root of the binary search tree (assumed to be valid)
+     * @param target the value to search for
+     * @return true if the target is contained in the tree, false otherwise
+     */
+    public static <T extends Comparable<T>> boolean contains(BinaryTreeNode<T> root, T target) {
+        return false;
+    }
+}
