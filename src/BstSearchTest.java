83080369b5bb5f39dc790f712408d05de62591b0
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-10-28 11:35:45 -0700

    init test

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+    @Test
+    void BasicTree(){
+        BinaryTreeNode<Integer> leftLeaf = new BinaryTreeNode<>(2);
+        BinaryTreeNode<Integer> rightLeaf = new BinaryTreeNode<>(3);
+        
+        // Create a root node with left and right children
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1, leftLeaf, rightLeaf);
 
+        assertTrue(BstSearch.contains(root, 2));
+    }
 }
