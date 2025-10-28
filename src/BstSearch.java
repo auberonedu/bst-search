374130398e389bcea58584825e8a17c560964f24
Auberon López <auberon.lopez@greenriver.edu>
2025-10-28 09:38:33 -0700

    Added NPE docs

--- src/BstSearch.java
+++ src/BstSearch.java
@@ -8,12 +8,13 @@ public class BstSearch {
      * 
      * A null value for the root is considered to represent a valid but empty
      * binary search tree. A null root will cause this method to always return
-     * false.
+     * false if a valid target is given.
      * 
      * @param <T> The type of the values held in the nodes
      * @param root the root of the binary search tree (assumed to be valid)
      * @param target the value to search for
      * @return true if the target is contained in the tree, false otherwise
+     * @throws NullPointerException if target is null
      */
     public static <T extends Comparable<T>> boolean contains(BinaryTreeNode<T> root, T target) {
         return false;
