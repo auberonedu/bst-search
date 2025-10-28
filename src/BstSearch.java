20c6c7f9784d37d0c820f424301271a5ff4006b6
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-10-28 11:46:14 -0700

    fixing to catch null inputs

--- src/BstSearch.java
+++ src/BstSearch.java
@@ -17,16 +17,19 @@ public class BstSearch {
      * @throws NullPointerException if target is null
      */
     public static <T extends Comparable<T>> boolean contains(BinaryTreeNode<T> root, T target) {
-
+        if(target == null){
+            throw new NullPointerException();
+        }
         
 
         return dfsContains(root, target);
     }
 
     public static <T extends Comparable<T>> boolean dfsContains(BinaryTreeNode<T> node, T target){
+        if(node.data == null) return false;
         boolean check = false;
         if(node.data.compareTo(target) == 0){
-            return true;
+            check = true;
         }else if(node.data.compareTo(target)>0){
             check = dfsContains(node.left, target);
         }else{
