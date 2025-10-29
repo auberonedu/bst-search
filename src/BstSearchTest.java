a3694946ed1f9dabfd7ef0c542fd98a1cfabb31e
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-10-28 20:51:54 -0700

    Added more negative tests

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,6 +2,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+
+    //
+    // Positive Checks
+    //
+
     @Test
     public void BSTSearchTestResult_NullChecks_NoBinaryTree() {
         BstSearch search = new BstSearch();
@@ -95,19 +100,91 @@ public class BstSearchTest {
 
 
     @Test
-    public void BSTSearchTestResult_target_FindsNegative() {
+    public void BSTSearchTestResult_target_FindsNegative_OnLeft() {
         BinaryTreeNode tree = new BinaryTreeNode(-2);
         BstSearch search = new BstSearch();
 
-        tree.left = new BinaryTreeNode(-3);
+        tree.left = new BinaryTreeNode(-4);
         tree.left.left = new BinaryTreeNode(-5);
-        tree.left.right = new BinaryTreeNode(-4);
+        tree.left.right = new BinaryTreeNode(-3);
 
-        tree.right = new BinaryTreeNode(-1);
+        tree.right = new BinaryTreeNode(0);
         tree.right.right = new BinaryTreeNode(1);
-        tree.right.left = new BinaryTreeNode(0);
+        tree.right.left = new BinaryTreeNode(-1);
 
-        assertTrue(search.contains(tree, -5));        
+        assertTrue(search.contains(tree, -3));        
+    }
+
+    @Test
+    public void BSTSearchTestResult_target_FindsNegative_OnRight() {
+        BinaryTreeNode tree = new BinaryTreeNode(-2);
+        BstSearch search = new BstSearch();
+
+        tree.left = new BinaryTreeNode(-4);
+        tree.left.left = new BinaryTreeNode(-5);
+        tree.left.right = new BinaryTreeNode(-3);
+
+        tree.right = new BinaryTreeNode(0);
+        tree.right.right = new BinaryTreeNode(1);
+        tree.right.left = new BinaryTreeNode(-1);
+
+        assertTrue(search.contains(tree, -1));        
+    }
+
+    @Test
+    public void BSTSearchTestResult_target_FindsNegative_EqualRootAndTarget() {
+        BinaryTreeNode tree = new BinaryTreeNode(-2);
+        BstSearch search = new BstSearch();
+
+        tree.left = new BinaryTreeNode(-4);
+        tree.left.left = new BinaryTreeNode(-5);
+        tree.left.right = new BinaryTreeNode(-3);
+
+        tree.right = new BinaryTreeNode(0);
+        tree.right.right = new BinaryTreeNode(1);
+        tree.right.left = new BinaryTreeNode(-1);
+
+        assertTrue(search.contains(tree, -2));        
+    }
+
+    @Test
+    public void BSTSearchTestResult_target_FindsNegative_NoFoundTarget() {
+        BinaryTreeNode tree = new BinaryTreeNode(-2);
+        BstSearch search = new BstSearch();
+
+        tree.left = new BinaryTreeNode(-4);
+        tree.left.left = new BinaryTreeNode(-5);
+        tree.left.right = new BinaryTreeNode(-3);
+
+        tree.right = new BinaryTreeNode(0);
+        tree.right.right = new BinaryTreeNode(1);
+        tree.right.left = new BinaryTreeNode(-1);
+
+        assertFalse(search.contains(tree, -10));        
+    }
+
+
+    @Test
+    public void BSTSearchTestResult_target_FindsNegative_NullRoot() {
+        BstSearch search = new BstSearch();
+
+        assertFalse(search.contains(null, -2));        
+    }
+
+    @Test
+    public void BSTSearchTestResult_target_FindsNegative_NullTarget() {
+        BinaryTreeNode tree = new BinaryTreeNode(-2);
+        BstSearch search = new BstSearch();
+
+        tree.left = new BinaryTreeNode(-4);
+        tree.left.left = new BinaryTreeNode(-5);
+        tree.left.right = new BinaryTreeNode(-3);
+
+        tree.right = new BinaryTreeNode(0);
+        tree.right.right = new BinaryTreeNode(1);
+        tree.right.left = new BinaryTreeNode(-1);
+
+        assertThrows(NullPointerException.class, () -> search.contains(tree, null));        
     }
     
 }
