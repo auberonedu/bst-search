389063fbf3caab735eecd818dea97752ca7949a2
Tim <timmywilliams4665@gmail.com>
2025-10-28 20:32:16 -0700

    Found fix

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -3,6 +3,13 @@ import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
     @Test
+    public void BSTSearchTestResult_NullChecks_NoBinaryTree() {
+        BstSearch search = new BstSearch();
+
+        assertFalse(search.contains(null, 4));        
+    }
+    
+    @Test
     public void BSTSearchTestResult_target_4() {
         BinaryTreeNode tree = new BinaryTreeNode(6);
         BstSearch search = new BstSearch();
@@ -16,13 +23,5 @@ public class BstSearchTest {
         tree.right.left = new BinaryTreeNode(7);
 
         assertTrue(search.contains(tree, 4));        
-    }
-
-    @Test
-    public void BSTSearchTestResult_NullChecks() {
-        BinaryTreeNode tree = new BinaryTreeNode(null);
-        BstSearch search = new BstSearch();
-
-        assertFalse(search.contains(tree, 4));        
     }
 }
