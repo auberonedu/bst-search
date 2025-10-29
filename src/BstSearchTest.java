1d541f2aeb066dd8d423c79d1552ec7ae048248b
Tim <timmywilliams4665@gmail.com>
2025-10-28 20:34:09 -0700

    Finished null checks

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -8,6 +8,38 @@ public class BstSearchTest {
 
         assertFalse(search.contains(null, 4));        
     }
+
+    @Test
+    public void BSTSearchTestResult_NullChecks_NoTarget() {
+        BinaryTreeNode tree = new BinaryTreeNode(6);
+        BstSearch search = new BstSearch();
+
+        tree.left = new BinaryTreeNode(4);
+        tree.left.left = new BinaryTreeNode(3);
+        tree.left.right = new BinaryTreeNode(5);
+
+        tree.right = new BinaryTreeNode(8);
+        tree.right.right = new BinaryTreeNode(9);
+        tree.right.left = new BinaryTreeNode(7);
+
+        assertFalse(search.contains(tree, 10));        
+    }
+
+    @Test
+    public void BSTSearchTestResult_RootIsTarget() {
+        BinaryTreeNode tree = new BinaryTreeNode(6);
+        BstSearch search = new BstSearch();
+
+        tree.left = new BinaryTreeNode(4);
+        tree.left.left = new BinaryTreeNode(3);
+        tree.left.right = new BinaryTreeNode(5);
+
+        tree.right = new BinaryTreeNode(8);
+        tree.right.right = new BinaryTreeNode(9);
+        tree.right.left = new BinaryTreeNode(7);
+
+        assertTrue(search.contains(tree, 6));        
+    }
     
     @Test
     public void BSTSearchTestResult_target_4() {
