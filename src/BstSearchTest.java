b2b773a414878e3f2b26afe079a5ff2a3660a643
VibeSyntax <138536097+beza102@users.noreply.github.com>
2025-10-28 19:46:07 -0700

    added a test case

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -1,6 +1,32 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+    //helper method
+    private BinaryTreeNode<Integer> createSampleTree(){
+        //create nodes
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(10);
+        root.left = new BinaryTreeNode<Integer>(5);
+        root.right = new BinaryTreeNode<Integer>(15);
+        root.left.left = new BinaryTreeNode<Integer>(3);
+        root.left.right = new BinaryTreeNode<Integer>(7);
+        root.right.right = new BinaryTreeNode<Integer>(20);
+         return root;
+
+    }
+    @Test
+    public void testContainValueDoesNotExist(){
+        BinaryTreeNode<Integer> root = createSampleTree();
+
+        //not in tree
+        assertFalse(BstSearch.contains(root, 8));
+        //smaller
+        assertFalse(BstSearch.contains(root, 1));
+        //larger
+        assertFalse(BstSearch.contains(root, 30));
+
+    }
 
 }
