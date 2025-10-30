76626f27050b09d453b3206a5198f2ec09ac386d
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-10-29 21:55:52 -0700

    Created test to return true if value exists

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+    @Test
+    void contains_returnsTrue_whenValueExists() {
+        //Arrange
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(8);
+        root.left = new BinaryTreeNode<>(3);
+        root.right = new BinaryTreeNode<>(10);
+        root.left.right = new BinaryTreeNode<>(6);
+        //Act
+        boolean result = BstSearch.contains(root, 6);
+        //Assert
+        assertTrue(result);
+    }
 
+    
+    
 }
